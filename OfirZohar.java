0248c47e501beff3706344e01df636588fce0653
ofirzohar11 <ofirzohar11@gmail.com>
2025-11-16 20:32:22 +0200

    Hello.java,OfirZohar.java

---
+++ OfirZohar.java
@@ -0,0 +1,7 @@
+public class Ofir {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ofir Zohar - run #" + (i + 1));
+        }
+    }
+}
