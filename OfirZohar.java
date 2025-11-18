34c36c152616b50e20de3509c9afd4850519fad2
ofirzohar11 <ofirzohar11@gmail.com>
2025-11-18 21:57:43 +0200

    update OfirZohar.java

--- OfirZohar.java
+++ OfirZohar.java
@@ -1,4 +1,4 @@
-public class Ofir {
+public class OfirZohar {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("2- 2 #" + (i + 1));
